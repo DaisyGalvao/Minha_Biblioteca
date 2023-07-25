2b18fd9ecb7d09aca6afa1563884edfa53a5201f
Daisy Galvão <daisy.smgalvao@gmail.com>
2023-07-24 22:30:21 -0300

    CreateStatusLivro

--- Minha_Biblioteca/Controllers/StatusLivroController.cs
+++ Minha_Biblioteca/Controllers/StatusLivroController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Minha_Biblioteca.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,25 @@ namespace Minha_Biblioteca.Controllers
     [Route("[controller]")]
     public class StatusLivroController : ControllerBase
     {
+        [HttpPost]
+        [Route("Create")]
+        public void Create(StatusLivroModel statuslivroModel)
+        {
+            var query = @"insert into statuslivro values
+                        (
+                         @Nome
+                        )";
+            using (var conn = new SqlConnection("Server=DAISY\\SQLEXPRESS; Database=Minha_Biblioteca; User Id=sa; Password=anonimo;"))
+            {
+                conn.Open();
+
+                conn.Query(query,
+                    new
+                    {
+                        Nome = statuslivroModel.Nome
+                    }).FirstOrDefault();
+            }
+        }
     }
 }
+
